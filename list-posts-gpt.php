5af95b5e272d009ba7cac9eb6840b42c045513ba
Italo Mariano <84940616+italomsr@users.noreply.github.com>
2022-12-07 21:11:36 -0300

    Update list-posts-gpt.php

--- list-posts-gpt.php
+++ list-posts-gpt.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: List CAT TAG
+Plugin Name: List Posts
 Plugin URI: https://example.com/plugins/list-posts
 Description: A plugin that lists the categories and tags of the last 10 posts and exports them to a CSV file.
 Version: 1.0.0
